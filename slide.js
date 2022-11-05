bfcaa0e8c9a3b187477ddd2ee38b651cb516dd40
Hitansh Thakur <thakurhitansh4325@gmail.com>
2022-11-05 21:15:40 +0530

    increase in time interval

--- slide.js
+++ slide.js
@@ -2,7 +2,7 @@ const slides = document.querySelectorAll('.slide');
 const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
 const auto = true;
-const intervalTime = 5000;
+const intervalTime = 6000;
 let slideInterval;
 
 const nextSlide  = () =>{
